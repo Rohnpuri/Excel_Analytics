699257d103cf4ec06479eb3744c20abbde88b987
Gaurav775-Git <gaurav775.official@gmail.com>
2025-06-29 22:49:03 +0530

    initialise commit

---
+++ excel-analytics-backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config();
+
+// Initialize app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB Connection
+connectDB();
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
